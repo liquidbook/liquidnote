d6e02ed20b0e935f676218e17fb60a38fd81d649
Felipe Lujan-Bear <felipe@liquidbook.com>
2014-05-14 17:04:00 -0700

    refine liquidnote-script moves content to trigger
    New liquidnote-script move the trigger_container to the trigger link on
    show (toggle), and moves it to the end of the body on hide. Also the
    appendto and the insertAfter stop the media from playing - nice plus -
    so I remove the iframe src reset trick. Code is smaller and more
    flexible.

--- js/liquidnote-script.js
+++ js/liquidnote-script.js
@@ -5,43 +5,27 @@ jQuery(document).ready(function ($) { //noconflict wrapper
     function () {
         var trigger_id = $(this).attr('id');
         var trigger_container = '.' + trigger_id;
+        var trigger = '#' + trigger_id;
         var my_id = trigger_id.substring(6, trigger_id.length);
 
-        if ($(trigger_container).hasClass("lnvideo")) {
-            var video_id = "#video" + my_id;
-            var video = $(video_id).attr("src");
-            //console.log("it has a video class");
-            $(video_id).attr('src', '');
-            $(video_id).attr('src', video);
+        if($(trigger_container).is(":hidden")){
+            	$(trigger_container).insertAfter(trigger);
+				$(trigger_container).toggle('slow');
+        } else if($(trigger_container).is(":visible")) {
+            	$(trigger_container).toggle('fast', function(){
+				$(trigger_container).appendTo("body");
+            });
         }
-
-        if ($(trigger_container).hasClass("lnsound")) {
-            //var video_id = "#video" + my_id;
-            var sound_src = $(trigger_container + " iframe").attr("src");
-            //console.log("it has a sound class");
-            //console.log(sound_src);
-            $(trigger_container + ' iframe').attr('src', '');
-            $(trigger_container + ' iframe').attr('src', sound_src);
-            console.log($(trigger_container));
-        }
-
-        $(trigger_container).toggle('slow');
-        //console.log(video);
         return false;
     }); // end .annotate-button.click
     $('.annotate-closebox').click(
 
     function () {
         var trigger_container = $(this).parent().get(0);
-        var trigger_src = $(this).parent().find('iframe').attr("src");
-        $(trigger_container).toggle('fast');
-        if ($(this).parent().has('iframe')) {
-            $(this).parent().find('iframe').attr('src', '');
-            $(this).parent().find('iframe').attr('src', trigger_src);
-            console.log(trigger_src);
-        }
-
-
+        var trigger_src = $(this).parent().find('iframe').attr("src");        
+        $(trigger_container).toggle('fast', function() {
+                $(trigger_container).appendTo("body");
+            }); 
         return false;
     }); // end .annotate-button.click      
 
